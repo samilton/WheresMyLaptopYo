b04f59554fd052da72c563a96b23f7fd9fb5079a
Sam Hamilton <shamilton@pragmatrading.com>
2012-11-30 19:54:42 -0500

    Initial Commit

---
+++ WhereIsMyLaptop/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WhereIsMyLaptop
+//
+//  Created by Sam Hamilton on 11/30/12.
+//  Copyright (c) 2012 Sam Hamilton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
